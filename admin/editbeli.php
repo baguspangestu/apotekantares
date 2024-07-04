26c83126d69e90bdeab506781942ce146d9ad00e
Bagus Pangestu <baguspangestu@yandex.com>
2024-07-05 00:32:54 +0700

    Fix Bug

--- admin/editbeli.php
+++ admin/editbeli.php
@@ -29,21 +29,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $listPembelianAwal = json_decode($jsonListPembelianAwal, true);
   $listPembelianEdit = json_decode($_POST['dataListPembelian'], true);
 
-  $listPembelianKeys = array_column($listPembelianEdit, null, 'kd');
+  $listPembelianAwalKeys = array_column($listPembelianAwal, null, 'kd');
+  $listPembelianEditKeys = array_column($listPembelianEdit, null, 'kd');
 
   foreach ($listPembelianAwal as &$itemAwal) {
     $key = $itemAwal['kd'];
-
-    if (!isset($listPembelianKeys[$key])) {
-      $itemAwal['jumlah'] = 0;
-    }
+    $isE = isset($listPembelianEditKeys[$key]);
+    $itemAwal['jumlah'] = $isE ? $listPembelianEditKeys[$key]['jumlah'] : 0;
   }
 
-  $listPembelian = array_merge($listPembelianAwal, $listPembelianEdit);
+  foreach ($listPembelianEdit as $itemEdit) {
+    $key = $itemEdit['kd'];
+    $isA = isset($listPembelianAwalKeys[$key]);
+    if (!$isA) $listPembelianAwal[] = $itemEdit;
+  }
 
-  usort($listPembelian, function ($a, $b) {
-    return $a['kd'] <=> $b['kd'];
-  });
+  $listPembelian = array_values($listPembelianAwal);
 
   $konek->begin_transaction();
 
@@ -98,8 +99,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<div class="modal fade" id="listProdukModal" tabindex="-1" role="dialog" aria-labelledby="listProdukModalLabel"
-  aria-hidden="true">
+<div class="modal fade" id="listProdukModal" tabindex="-1" role="dialog" aria-labelledby="listProdukModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -140,8 +140,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
   <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-dolly-flatbed"></i> Data Pembelian</h1>
 
-  <a href="?page=databeli" class="btn btn-secondary btn-icon-split"><span class="icon text-white-50"><i
-        class="fas fa-arrow-left"></i></span>
+  <a href="?page=databeli" class="btn btn-secondary btn-icon-split"><span class="icon text-white-50"><i class="fas fa-arrow-left"></i></span>
     <span class="text">Kembali</span>
   </a>
 </div>
@@ -162,9 +161,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $qq = mysqli_query($konek, "SELECT * FROM suplier ORDER BY nama ASC");
             while ($dd = mysqli_fetch_assoc($qq)) {
             ?>
-            <option value="<?php echo $dd['kd']; ?>"
-              <?php echo $transaksiAwalData['kd_suplier'] == $dd['kd'] ? 'selected' : '' ?>>
-              <?php echo $dd['nama'] ?></option>
+              <option value="<?php echo $dd['kd']; ?>" <?php echo $transaksiAwalData['kd_suplier'] == $dd['kd'] ? 'selected' : '' ?>>
+                <?php echo $dd['nama'] ?></option>
             <?php
             }
             ?>
@@ -172,8 +170,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
         <div class="form-group col-md-6">
           <label class="font-weight-bold">Tanggal Transaksi</label>
-          <input autocomplete="off" type="date" name="tanggal" value="<?php echo $transaksiAwalData['tanggal']; ?>"
-            class="form-control" required />
+          <input autocomplete="off" type="date" name="tanggal" value="<?php echo $transaksiAwalData['tanggal']; ?>" class="form-control" required />
         </div>
       </div>
       <hr />
@@ -269,212 +266,212 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <script src="../assets/js/helpers.js"></script>
 <script src="../assets/vendor/jquery/jquery.min.js"></script>
 <script>
-const listProduk = [];
-const listPembelianAwal = <?php echo $jsonListPembelianAwal ?>;
-const listPembelian = <?php echo $jsonListPembelianAwal ?>;
+  const listProduk = [];
+  const listPembelianAwal = <?php echo $jsonListPembelianAwal ?>;
+  const listPembelian = <?php echo $jsonListPembelianAwal ?>;
 
-let selectedProduct = null;
-let subTotal = 0;
-let grandTotal = 0;
+  let selectedProduct = null;
+  let subTotal = 0;
+  let grandTotal = 0;
 
-function formatTanggal(tanggal) {
-  let bagian = tanggal.split('-');
-  return bagian[2] + '-' + bagian[1] + '-' + bagian[0];
-}
+  function formatTanggal(tanggal) {
+    let bagian = tanggal.split('-');
+    return bagian[2] + '-' + bagian[1] + '-' + bagian[0];
+  }
 
-function getListProduk(q = '', p = 1) {
-  $.ajax({
-    url: 'data_produk_suplier.php',
-    method: 'GET',
-    data: {
-      kd_suplier: $("#kd_suplier").val(),
-      q,
-      p
-    },
-    dataType: 'json',
-    success: function(res) {
-      listProduk.length = 0;
-      $('#tbodyListProduk').empty();
-      $('#pagingListProduk').empty();
+  function getListProduk(q = '', p = 1) {
+    $.ajax({
+      url: 'data_produk_suplier.php',
+      method: 'GET',
+      data: {
+        kd_suplier: $("#kd_suplier").val(),
+        q,
+        p
+      },
+      dataType: 'json',
+      success: function(res) {
+        listProduk.length = 0;
+        $('#tbodyListProduk').empty();
+        $('#pagingListProduk').empty();
 
-      if (res.data.length) {
-        res.data.forEach((e, index) => {
-          listProduk.push(e);
+        if (res.data.length) {
+          res.data.forEach((e, index) => {
+            listProduk.push(e);
 
+            const row = $('<tr></tr>').appendTo('#tbodyListProduk');
+            row.addClass("text-center");
+
+            const cell = [];
+            for (let i = 0; i < 8; i++) {
+              const c = $('<td></td>').appendTo(row);
+              c.addClass("align-middle");
+
+              if (i === 2) c.addClass("text-left");
+              if (i === 5) c.addClass("text-right");
+
+              cell.push(c);
+            }
+
+            cell[0].text(index + 1);
+            cell[1].text(e.kd);
+            cell[2].text(e.nama);
+            cell[3].text(formatTanggal(e.tgl_exp));
+            cell[4].text(e.satuan);
+            cell[5].text(formatRupiah(e.harga));
+            cell[6].text(e.stok);
+            cell[7].html(`<span class="btn btn-success btn-sm" onClick="pilihProduk('${e.kd}')">Pilih</span>`);
+          });
+        } else {
           const row = $('<tr></tr>').appendTo('#tbodyListProduk');
           row.addClass("text-center");
+          const cell1 = $('<td></td>').appendTo(row);
+          cell1.attr('colspan', 8);
+          cell1.text("Produk tidak ditemukan");
+        }
 
-          const cell = [];
-          for (let i = 0; i < 8; i++) {
-            const c = $('<td></td>').appendTo(row);
-            c.addClass("align-middle");
-
-            if (i === 2) c.addClass("text-left");
-            if (i === 5) c.addClass("text-right");
-
-            cell.push(c);
-          }
-
-          cell[0].text(index + 1);
-          cell[1].text(e.kd);
-          cell[2].text(e.nama);
-          cell[3].text(formatTanggal(e.tgl_exp));
-          cell[4].text(e.satuan);
-          cell[5].text(formatRupiah(e.harga));
-          cell[6].text(e.stok);
-          cell[7].html(`<span class="btn btn-success btn-sm" onClick="pilihProduk('${e.kd}')">Pilih</span>`);
-        });
-      } else {
-        const row = $('<tr></tr>').appendTo('#tbodyListProduk');
-        row.addClass("text-center");
-        const cell1 = $('<td></td>').appendTo(row);
-        cell1.attr('colspan', 8);
-        cell1.text("Produk tidak ditemukan");
-      }
-
-      $('#pagingListProduk').append(`
+        $('#pagingListProduk').append(`
         <li class="page-item ${res.page === 1 ? 'disabled' : ''}">
           <a class="page-link" href="#" tabindex="-1" onclick="getListProduk('${q}', ${res.page - 1})">Previous</a>
         </li>
       `);
 
-      let startPage = Math.max(res.page - 1, 1);
-      let endPage = Math.min(res.page + 1, res.pages);
-      for (let i = startPage; i <= endPage; i++) {
-        $('#pagingListProduk').append(`
+        let startPage = Math.max(res.page - 1, 1);
+        let endPage = Math.min(res.page + 1, res.pages);
+        for (let i = startPage; i <= endPage; i++) {
+          $('#pagingListProduk').append(`
           <li class="page-item ${i === res.page ? 'active' : ''}">
             <a class="page-link" href="#" onclick="getListProduk('${q}', ${i})">${i}</a>
           </li>
         `);
-      }
+        }
 
-      $('#pagingListProduk').append(`
+        $('#pagingListProduk').append(`
         <li class="page-item ${res.page >= res.pages ? 'disabled' : ''}">
           <a class="page-link" href="#" onclick="getListProduk('${q}', ${res.page + 1})">Next</a>
         </li>
       `);
-    }
-  });
-}
-
-function resetTPForm() {
-  $("#tpKdInput").val('');
-  $("#tpNamaInput").val('');
-  $("#tpTglExp").val('');
-  $("#tpSatuan").text('');
-  $("#tpHargaInput").val('');
-  $("#tpJumlahInput").val('');
-  $("#tpTotalInput").val('');
-  $('#tpSubmitButton').addClass('disabled');
-}
-
-function pilihProduk(kd) {
-  $('#listProdukModal').modal('hide');
-  selectedProduct = listProduk.find(e => e.kd === kd) || null;
-
-  $("#tpKdInput").val(selectedProduct?.kd ?? '-');
-  $("#tpNamaInput").val(selectedProduct?.nama ?? '-');
-  $("#tpTglExp").val(selectedProduct?.tgl_exp ?? '');
-  $("#tpSatuan").text(selectedProduct?.satuan ?? '');
-  $("#tpHargaInput").val(formatRupiah(selectedProduct?.harga ?? 0));
-  $("#tpJumlahInput").val(selectedProduct?.harga ? 1 : 0);
-
-  tpHitungTotal();
-}
-
-function tpHitungTotal() {
-  const jumlah = parseInt($("#tpJumlahInput").val()) || 0;
-  const total = jumlah * selectedProduct?.harga ?? 0;
-  $("#tpTotalInput").val(formatRupiah(!isNaN(total) ? total : 0));
-
-  if (jumlah >= 0) {
-    $("#tpJumlahInput").removeClass("is-invalid");
-  } else {
-    $("#tpJumlahInput").addClass('is-invalid');
-  }
-
-  if (selectedProduct && jumlah >= 1) {
-    $('#tpSubmitButton').removeClass('disabled');
-  } else {
-    $('#tpSubmitButton').addClass('disabled');
-  }
-}
-
-function tambahkanPembelian() {
-  if ($('#tpSubmitButton').hasClass('disabled')) return;
-
-  const jumlah = parseInt($("#tpJumlahInput").val());
-  const i = listPembelian.findIndex(item => item.kd === selectedProduct.kd);
-
-  if (i !== -1) {
-    listPembelian[i].jumlah = parseInt(listPembelian[i].jumlah) + jumlah;
-  } else {
-    listPembelian.push({
-      ...selectedProduct,
-      jumlah
+      }
     });
   }
 
-  selectedProduct = null;
-  resetTPForm();
-  updateDaftarPembelian();
-}
+  function resetTPForm() {
+    $("#tpKdInput").val('');
+    $("#tpNamaInput").val('');
+    $("#tpTglExp").val('');
+    $("#tpSatuan").text('');
+    $("#tpHargaInput").val('');
+    $("#tpJumlahInput").val('');
+    $("#tpTotalInput").val('');
+    $('#tpSubmitButton').addClass('disabled');
+  }
 
-function updatePembelian(kd, jumlah) {
-  const index = listPembelian.findIndex(item => item.kd === kd);
-  if (index !== -1) listPembelian[index].jumlah = jumlah;
+  function pilihProduk(kd) {
+    $('#listProdukModal').modal('hide');
+    selectedProduct = listProduk.find(e => e.kd === kd) || null;
 
-  updateDaftarPembelian();
-}
+    $("#tpKdInput").val(selectedProduct?.kd ?? '-');
+    $("#tpNamaInput").val(selectedProduct?.nama ?? '-');
+    $("#tpTglExp").val(selectedProduct?.tgl_exp ?? '');
+    $("#tpSatuan").text(selectedProduct?.satuan ?? '');
+    $("#tpHargaInput").val(formatRupiah(selectedProduct?.harga ?? 0));
+    $("#tpJumlahInput").val(selectedProduct?.harga ? 1 : 0);
 
-function removePembelian(kd) {
-  const confirmed = confirm("Apakah Anda yakin ingin menghapus data ini?")
-  if (!confirmed) return;
+    tpHitungTotal();
+  }
 
-  const index = listPembelian.findIndex(item => item.kd === kd);
-  if (index !== -1) listPembelian.splice(index, 1);
+  function tpHitungTotal() {
+    const jumlah = parseInt($("#tpJumlahInput").val()) || 0;
+    const total = jumlah * selectedProduct?.harga ?? 0;
+    $("#tpTotalInput").val(formatRupiah(!isNaN(total) ? total : 0));
 
-  updateDaftarPembelian();
-}
+    if (jumlah >= 0) {
+      $("#tpJumlahInput").removeClass("is-invalid");
+    } else {
+      $("#tpJumlahInput").addClass('is-invalid');
+    }
 
-function updateDaftarPembelian() {
-  $('#tbodyListPembelian').empty();
-  subTotal = 0;
+    if (selectedProduct && jumlah >= 1) {
+      $('#tpSubmitButton').removeClass('disabled');
+    } else {
+      $('#tpSubmitButton').addClass('disabled');
+    }
+  }
 
-  $('#dataListPembelian').val(JSON.stringify(listPembelian));
+  function tambahkanPembelian() {
+    if ($('#tpSubmitButton').hasClass('disabled')) return;
 
-  if (listPembelian.length) {
-    $("#submit").removeClass('disabled');
+    const jumlah = parseInt($("#tpJumlahInput").val());
+    const i = listPembelian.findIndex(item => item.kd === selectedProduct.kd);
 
-    listPembelian.forEach((e, index) => {
-      const total = e.harga * e.jumlah;
+    if (i !== -1) {
+      listPembelian[i].jumlah = parseInt(listPembelian[i].jumlah) + jumlah;
+    } else {
+      listPembelian.push({
+        ...selectedProduct,
+        jumlah
+      });
+    }
 
-      const row = $('<tr></tr>').appendTo('#tbodyListPembelian');
-      row.addClass("text-center");
+    selectedProduct = null;
+    resetTPForm();
+    updateDaftarPembelian();
+  }
 
-      const cell = [];
-      for (let i = 0; i < 9; i++) {
-        const c = $('<td></td>').appendTo(row);
-        c.addClass("align-middle");
+  function updatePembelian(kd, jumlah) {
+    const index = listPembelian.findIndex(item => item.kd === kd);
+    if (index !== -1) listPembelian[index].jumlah = jumlah;
 
-        if (i === 2) {
-          c.addClass("text-left");
-        } else if (i === 5 || i === 7) {
-          c.addClass("text-right");
-        } else if (i === 6) {
-          c.addClass("d-flex justify-content-between align-items-center");
+    updateDaftarPembelian();
+  }
+
+  function removePembelian(kd) {
+    const confirmed = confirm("Apakah Anda yakin ingin menghapus data ini?")
+    if (!confirmed) return;
+
+    const index = listPembelian.findIndex(item => item.kd === kd);
+    if (index !== -1) listPembelian.splice(index, 1);
+
+    updateDaftarPembelian();
+  }
+
+  function updateDaftarPembelian() {
+    $('#tbodyListPembelian').empty();
+    subTotal = 0;
+
+    $('#dataListPembelian').val(JSON.stringify(listPembelian));
+
+    if (listPembelian.length) {
+      $("#submit").removeClass('disabled');
+
+      listPembelian.forEach((e, index) => {
+        const total = e.harga * e.jumlah;
+
+        const row = $('<tr></tr>').appendTo('#tbodyListPembelian');
+        row.addClass("text-center");
+
+        const cell = [];
+        for (let i = 0; i < 9; i++) {
+          const c = $('<td></td>').appendTo(row);
+          c.addClass("align-middle");
+
+          if (i === 2) {
+            c.addClass("text-left");
+          } else if (i === 5 || i === 7) {
+            c.addClass("text-right");
+          } else if (i === 6) {
+            c.addClass("d-flex justify-content-between align-items-center");
+          }
+
+          cell.push(c);
         }
 
-        cell.push(c);
-      }
-
-      cell[0].text(index + 1);
-      cell[1].text(e.kd);
-      cell[2].text(e.nama);
-      cell[3].text(formatTanggal(e.tgl_exp));
-      cell[4].text(e.satuan);
-      cell[5].text(formatRupiah(e.harga));
-      cell[6].html(`
+        cell[0].text(index + 1);
+        cell[1].text(e.kd);
+        cell[2].text(e.nama);
+        cell[3].text(formatTanggal(e.tgl_exp));
+        cell[4].text(e.satuan);
+        cell[5].text(formatRupiah(e.harga));
+        cell[6].html(`
           <span class="btn btn-outline-success btn-sm ${parseInt(e.jumlah) - 1 <= 0 ? 'disabled' : ''}" 
                 onClick="${parseInt(e.jumlah) - 1 <= 0 ? '' : `updatePembelian('${e.kd}','${parseInt(e.jumlah) - 1}')`}">
               <i class="fas fa-chevron-left"></i>
@@ -485,54 +482,54 @@ function updateDaftarPembelian() {
               <i class="fas fa-chevron-right"></i>
           </span>
         `);
-      cell[7].text(formatRupiah(total));
-      cell[8].html(`
+        cell[7].text(formatRupiah(total));
+        cell[8].html(`
             <span style="cursor: pointer" class="text-danger" onClick="removePembelian('${e.kd}')"><i class="fas fa-trash-alt"></i></span>
         `);
 
-      subTotal += total;
-    });
-  } else {
-    $("#submit").addClass('disabled');
-    const tbody = $('#tbodyListPembelian');
-    const row = $('<tr></tr>').appendTo(tbody);
-    row.addClass("text-center");
-    const cell1 = $('<td></td>').appendTo(row);
-    cell1.attr('colspan', 9);
-    cell1.text("Belum ada data pembelian");
+        subTotal += total;
+      });
+    } else {
+      $("#submit").addClass('disabled');
+      const tbody = $('#tbodyListPembelian');
+      const row = $('<tr></tr>').appendTo(tbody);
+      row.addClass("text-center");
+      const cell1 = $('<td></td>').appendTo(row);
+      cell1.attr('colspan', 9);
+      cell1.text("Belum ada data pembelian");
+    }
+
+    $("#subTotalHidden").val(subTotal);
+    $("#subTotal").text(formatRupiah(subTotal));
   }
 
-  $("#subTotalHidden").val(subTotal);
-  $("#subTotal").text(formatRupiah(subTotal));
-}
+  $('#listProdukButton').click(function() {
+    if ($('#listProdukButton').hasClass('disabled')) return;
+    $('#listProdukModal').modal('show');
+    getListProduk();
+  });
 
-$('#listProdukButton').click(function() {
-  if ($('#listProdukButton').hasClass('disabled')) return;
-  $('#listProdukModal').modal('show');
-  getListProduk();
-});
+  $('#cariProduk').on('input', function() {
+    getListProduk($(this).val());
+  });
 
-$('#cariProduk').on('input', function() {
-  getListProduk($(this).val());
-});
+  $("#tpJumlahInput").on('input', tpHitungTotal);
 
-$("#tpJumlahInput").on('input', tpHitungTotal);
+  $('#tpSubmitButton').click(tambahkanPembelian);
 
-$('#tpSubmitButton').click(tambahkanPembelian);
+  $("#submit").click(function(e) {
+    if ($('#submit').hasClass('disabled')) return;
+    const confirmed = confirm("Apakah yakin ingin menyimpan data ini?")
+    if (confirmed) $('#transaksiForm').off('submit').submit();
+  })
 
-$("#submit").click(function(e) {
-  if ($('#submit').hasClass('disabled')) return;
-  const confirmed = confirm("Apakah yakin ingin menyimpan data ini?")
-  if (confirmed) $('#transaksiForm').off('submit').submit();
-})
-
-$("#reset").click(function(e) {
-  if (!listPembelian.length || JSON.stringify(listPembelianAwal) === JSON.stringify(listPembelian))
-    return location.reload();
-  const confirmed = confirm("Kamu yakin ingin mereset semua perubahan?")
-  if (confirmed) location.reload();
-})
+  $("#reset").click(function(e) {
+    if (!listPembelian.length || JSON.stringify(listPembelianAwal) === JSON.stringify(listPembelian))
+      return location.reload();
+    const confirmed = confirm("Kamu yakin ingin mereset semua perubahan?")
+    if (confirmed) location.reload();
+  })
 
 
-updateDaftarPembelian();
+  updateDaftarPembelian();
 </script>
